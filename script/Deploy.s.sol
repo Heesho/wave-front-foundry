b658baa057a9123792a83cdbda82bc7fedd70034
Heesho <hisham11027@gmail.com>
2025-07-30 23:20:02 -0400

    deployed on base sepolia but having issues verifying cause of via ir

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -23,11 +23,22 @@ contract Deploy is Script {
 
     function run() public {
         vm.startBroadcast();
+
         usdc = new MockUSDC();
+        console.log("MockUSDC deployed at:", address(usdc));
+
         tokenFactory = new TokenFactory();
+        console.log("TokenFactory deployed at:", address(tokenFactory));
+
         contentFactory = new ContentFactory();
+        console.log("ContentFactory deployed at:", address(contentFactory));
+
         saleFactory = new SaleFactory();
+        console.log("SaleFactory deployed at:", address(saleFactory));
+
         rewarderFactory = new RewarderFactory();
+        console.log("RewarderFactory deployed at:", address(rewarderFactory));
+
         waveFront = new WaveFront(
             address(usdc),
             address(tokenFactory),
@@ -35,8 +46,14 @@ contract Deploy is Script {
             address(contentFactory),
             address(rewarderFactory)
         );
+        console.log("WaveFront deployed at:", address(waveFront));
+
         waveFrontRouter = new WaveFrontRouter(address(waveFront));
+        console.log("Router deployed at:", address(waveFrontRouter));
+
         waveFrontMulticall = new WaveFrontMulticall(address(waveFront));
+        console.log("Multicall deployed at:", address(waveFrontMulticall));
+
         vm.stopBroadcast();
     }
 }
